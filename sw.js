5b157855d4ff49ff3541f2bb5c61ef3d161fcc7d
Hahahuhu050 <yogasadewa584@gmail.com>
2026-06-02 14:41:03 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,46 @@
+const CACHE_NAME = 'score-cekih-v1';
+const ASSETS_TO_CACHE = [
+  'index.html',
+  'style.css',
+  'app.js',
+  'manifest.json'
+];
+
+// Install Service Worker
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+      .then(() => self.skipWaiting())
+  );
+});
+
+// Activate Service Worker
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            return caches.delete(cache);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+// Fetch Assets offline-first handler
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response; // Return dari cache jika ada
+        }
+        return fetch(event.request); // Ambil dari network jika belum ada
+      })
+  );
+});
